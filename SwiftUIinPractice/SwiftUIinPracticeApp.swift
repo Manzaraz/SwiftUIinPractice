c00ca904bf08084fee9ee2386b6e3d122a77c57a
Christian Manzaraz <magnusmanz@icloud.com>
2025-03-31 21:49:16 -0300

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by Christian Manzaraz on 31/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
